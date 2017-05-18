97e17ae991edf1728712ab1d7b303ec1d3f43abd
Ania <Ania>
2017-05-18 17:59:17 +0200

    holded interval removed

--- scripts.js
+++ scripts.js
@@ -2,28 +2,29 @@ $(function(){
     var carouselList = $('#carousel ul');
 
     function changeSlide(){
-        carouselList.animate({marginLeft:-400}, 500, moveFirstSlideOnLastPlace);
+        carouselList.animate({marginLeft: -400}, 500, moveFirstSlideOnLastPlace);
     };
     function moveFirstSlideOnLastPlace(){
         carouselList.find('li:last').after(carouselList.find('li:first'));
-        carouselList.css({marginLeft:0});
+        carouselList.css({marginLeft: 0});
     };
     function changeSlideLeft(){
-        carouselList.animate({marginLeft:0}, 500);
+        carouselList.animate({marginLeft: 0}, 500);
     };
     function moveLastSlideOnFirstPlace(){
         carouselList.find('li:first').before(carouselList.find('li:last'));
-        carouselList.css({marginLeft:-400});
+        carouselList.css({marginLeft: -400});
         changeSlideLeft();
     };
-    var interval = setInterval(changeSlide,3000);
+    var interval = setInterval(changeSlide, 3000);
 
     $('.left-side').click(function() {
-        clearInterval(interval);
         changeSlide();
     });
     $('.right-side').click(function() {
-        clearInterval(interval);
         moveLastSlideOnFirstPlace();
+		clearInterval(interval);
+		moveFirstSlideOnLastPlace();
     });
 });
+
