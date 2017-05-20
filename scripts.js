74a6da91b6ea21d264480aff75d46f4550aa9d5a
Ania <Ania>
2017-05-20 21:07:57 +0200

    tabs removed, variables added

--- scripts.js
+++ scripts.js
@@ -1,31 +1,33 @@
 $(function(){
     var carouselList = $('#carousel ul');
+    var timeOut = 500;
+    var transitionDuration = 2000;
 
     function changeSlideLeft(){
-        carouselList.animate({marginLeft: -400}, 500, moveFirstSlideOnLastPlace);
+        carouselList.animate({marginLeft: -400}, timeOut, moveFirstSlideOnLastPlace);
     };
     function moveFirstSlideOnLastPlace(){
         carouselList.find('li:last').after(carouselList.find('li:first'));
         carouselList.css({marginLeft: 0});
     };
     function changeSlideRight(){
-        carouselList.animate({marginLeft: 0}, 500, moveLastSlideOnFirstPlace);
+        carouselList.animate({marginLeft: 0}, timeOut, moveLastSlideOnFirstPlace);
     };
     function moveLastSlideOnFirstPlace(){
         carouselList.find('li:first').before(carouselList.find('li:last'));
         carouselList.css({marginLeft: -400});
     };
-    var interval = setInterval(changeSlideLeft, 2000);
+    var interval = setInterval(changeSlideLeft, transitionDuration);
 
     $('.left-side').click(function() {
         clearInterval(interval);
-	    changeSlideLeft();
-	    interval = setInterval(changeSlideLeft, 2000);
+        changeSlideLeft();
+        interval = setInterval(changeSlideLeft, transitionDuration);
     });
     $('.right-side').click(function() {
-		clearInterval(interval);
-		changeSlideRight();
-        interval = setInterval(changeSlideRight, 2000);
-	});
+        clearInterval(interval);
+        changeSlideRight();
+        interval = setInterval(changeSlideRight, transitionDuration);
+    });
 });
 
